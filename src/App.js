5a610d385d0202fc280309d224f9f4ccf28c615c
Patricia <patricia.pereira@academico.ifpb.edu.br>
2022-05-08 23:46:10 -0300

    inicio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Formik ,Form, Field,ErrorMessage}from "formik";
+import * as yup from "yup";
+import React from "react";
 
 function App() {
+  const handleClickLogin = (values)=>console.log(values);
+  const validationLogin=yup.object().shape({
+     email: yup.string().email("o email não é valido").required(),
+     password:yup.string().min(8).required(),
+    });
+ 
+  const handleClickCadastro = (values)=> console.log(values);
+  const validationCadastro= yup.object().shape({
+    email: yup.string().email("o email não é valido").required(),
+    password:yup.string().min(8).required(),
+    confirmePassword:yup.string().oneOf([yup.ref("password"),null,"As senhas não são iguais."]),
+
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <h1>Login</h1>
+      <Formik 
+          initialValues={{}}onSubmit={handleClickLogin}
+          validationSchema={validationLogin}
+          
         >
-          Learn React
-        </a>
-      </header>
+          <Form className='login-form'>
+              <div className='login-form-group'>
+                <Field name="email"
+                className="form-field"
+                placeHolder="Email"></Field>
+
+                <ErrorMessage 
+                component="span"
+                name="email"
+                className="form-error"/>
+              </div>
+
+              <div className='login-form-group'>
+                <Field name="password"
+                className="form-field"
+                placeHolder="Senha"></Field>
+                <ErrorMessage 
+                component="span"
+                name="password"
+                className="form-error"/>
+              </div>
+              <button className="button"type="submit">Login</button>
+          </Form>
+        </Formik>
+     
+
+        <h1>Cadastro Usuario</h1>
+      <Formik 
+          initialValues={{}}onSubmit={handleClickCadastro}
+          validationSchema={validationCadastro}
+          
+        >
+          <Form className='cadastro-form'>
+              <div className='cadastro-form-group'>
+                <Field name="email"
+                className="form-field"
+                placeHolder="Email"></Field>
+
+                <ErrorMessage 
+                component="span"
+                name="email"
+                className="form-error"/>
+              </div>
+
+              <div className='login-form-group'>
+                <Field name="password"
+                className="form-field"
+                placeHolder="Senha"></Field>
+                <ErrorMessage 
+                component="span"
+                name="password"
+                className="form-error"/>
+              </div>
+
+              <div className='login-form-group'>
+                <Field name="confirmePassword"
+                className="form-field"
+                placeHolder="Confirme sua Senha"></Field>
+                <ErrorMessage 
+                component="span"
+                name="password"
+                className="form-error"/>
+              </div>
+              <button className="button"type="submit">Cadastro</button>
+          </Form>
+        </Formik>
     </div>
   );
 }
