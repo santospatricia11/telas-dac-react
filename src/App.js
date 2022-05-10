9c7d0e0c65f6a910936ef38e0920390553403a8e
Patricia <patricia.pereira@academico.ifpb.edu.br>
2022-05-10 20:29:14 -0300

    atualização

--- src/App.js
+++ src/App.js
@@ -5,53 +5,8 @@ import * as yup from "yup";
 import React from "react";
 
 function App() {
-  const handleClickLogin = (values)=>console.log(values);
-  const validationLogin=yup.object().shape({
-     email: yup.string().email("o email não é valido").required(),
-     password:yup.string().min(8).required(),
-    });
+  <p>ddddddddd</p>
  
-  const handleClickCadastro = (values)=> console.log(values);
-  const validationCadastro= yup.object().shape({
-    email: yup.string().email("o email não é valido").required(),
-    password:yup.string().min(8).required(),
-    confirmePassword:yup.string().oneOf([yup.ref("password"),null,"As senhas não são iguais."]),
-
-  });
-  return (
-    <div className="container">
-      <h1>Login</h1>
-      <Formik 
-          initialValues={{}}onSubmit={handleClickLogin}
-          validationSchema={validationLogin}
-          
-        >
-          <Form className='login-form'>
-              <div className='login-form-group'>
-                <Field name="email"
-                className="form-field"
-                placeHolder="Email"></Field>
-
-                <ErrorMessage 
-                component="span"
-                name="email"
-                className="form-error"/>
-              </div>
-
-              <div className='login-form-group'>
-                <Field name="password"
-                className="form-field"
-                placeHolder="Senha"></Field>
-                <ErrorMessage 
-                component="span"
-                name="password"
-                className="form-error"/>
-              </div>
-              <button className="button"type="submit">Login</button>
-          </Form>
-        </Formik>
-     
-
         <h1>Cadastro Usuario</h1>
       <Formik 
           initialValues={{}}onSubmit={handleClickCadastro}
